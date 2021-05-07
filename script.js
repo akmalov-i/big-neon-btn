74ed4ba0c808b61b965491b04a3771c3f1d60f16
akmalov-i <weberry.dev@gmail.com>
2021-05-07 19:03:13 +0500

    hover btn

--- script.js
+++ script.js
@@ -3,15 +3,20 @@ document.addEventListener("DOMContentLoaded", function () {
         enabled = document.querySelector('#enabled'),
         disabled = document.querySelector('#disabled')
 
-    btn.addEventListener('mouseenter', function () {
-        disabled.pause();
-        disabled.currentTime = 0;
-        enabled.play()
-    })
+    btn.addEventListener('click', function () {
+        btn.classList.add('active')
 
-    btn.addEventListener('mouseleave', function () {
-        enabled.pause();
-        enabled.currentTime = 0;
-        disabled.play()
+        if (btn.classList.contains('active')) {
+            disabled.pause();
+            disabled.currentTime = 0;
+            enabled.play()
+
+            btn.addEventListener('mouseleave', function () {
+                enabled.pause();
+                enabled.currentTime = 0;
+                disabled.play()
+                btn.classList.remove('active')
+            })
+        }
     })
 })
